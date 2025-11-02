cd959afc935f0db0aa83794a5338bf8729388c24
HosseinDakhili <hosseindakhili94@gmail.com>
2025-11-02 14:25:54 +0330

    ..

--- backend/Routes/Show.js
+++ backend/Routes/Show.js
@@ -1,7 +1,10 @@
 import express from "express";
 import { addShow, getNowPlayingMovie } from "../Controllers/ShowCn.js";
+import { isAdmin } from "../Middlewares/IsAdmin.js";
 
 const showRouter = express.Router();
-showRouter.get("/now-playing", getNowPlayingMovie).post('/add',addShow);
+showRouter
+  .get("/now-playing",isAdmin,getNowPlayingMovie)
+  .post("/add", isAdmin, addShow);
 
 export default showRouter;

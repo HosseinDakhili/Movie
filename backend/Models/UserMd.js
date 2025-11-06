aefcd7428e25f43a93d26b4a5b5826db4df4fa19
HosseinDakhili <hosseindakhili94@gmail.com>
2025-11-06 17:19:23 +0330

    update user schema to include required validation messages in Persian

--- backend/Models/UserMd.js
+++ backend/Models/UserMd.js
@@ -3,15 +3,23 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
-    required: [true, ""],
+    required: [true, "نام کاربری الزامی است"],
   },
   email: {
     type: String,
-    required: [true, ""],
+    required: [true, "ایمیل الزامی است"],
+  },
+  password: {
+    type: String,
+    required: [true, "رمز عبور الزامی است"],
   },
   image: {
     type: String,
-    
+  },
+  role: {
+    type: String,
+    enum: ["user", "admin", "superAdmin"],
+    default: "user",
   },
 });
 

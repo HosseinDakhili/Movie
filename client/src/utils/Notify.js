6c7079c8843fc51032c9fd668c4310a6ad9109cb
HosseinDakhili <hosseindakhili94@gmail.com>
2025-11-13 11:33:17 +0330

    feat: refactor main rendering structure to include AuthContextProvider; add utility functions for notifications and form handling

---
+++ client/src/utils/Notify.js
@@ -0,0 +1,10 @@
+import toast from "react-hot-toast";
+
+const notify = (type,msg) =>{
+    toast[type](msg,{
+        duration:4000,
+        position:'bottom-center'
+    })
+}
+
+export default notify

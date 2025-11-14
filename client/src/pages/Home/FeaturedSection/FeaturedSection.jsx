c85a077038075105bdffb73f97fd30bec8050e03
HosseinDakhili <hosseindakhili94@gmail.com>
2025-11-14 16:25:37 +0330

    feat: add time formatting utility and enhance MovieCard with runtime display and Persian localization in FeaturedSection

--- client/src/pages/Home/FeaturedSection/FeaturedSection.jsx
+++ client/src/pages/Home/FeaturedSection/FeaturedSection.jsx
@@ -1,25 +1,35 @@
 import { ArrowRight } from "lucide-react";
-import React from "react";
 import { useNavigate } from "react-router-dom";
 import BlurCircle from "../../../components/BlurCircle";
+import { dummyShowsData } from "../../../assets/assets";
+import MovieCard from "./MovieCard";
 
 export default function FeaturedSection() {
   const navigate = useNavigate();
+
   return (
-    <div className="px-6 md:px-16 lg:px-24 xl:px-44 overflow-hidden">
+    <div dir="rtl" className="px-6 md:px-16 lg:px-24 xl:px-44 overflow-hidden">
       <div className="relative flex items-center justify-between pt-20 pb-10">
         <BlurCircle top="0" left="-80px " />
-        <p className="text-gray-300 font-medium text-lg">Now Showing</p>
+
+        <p className="text-gray-300 font-medium text-lg">
+          در حال پخش
+        </p>
+
         <button
           onClick={() => navigate("/movies")}
           className="group flex items-center gap-2 text-sm text-gray-300 cursor-pointer"
         >
-          View All
-          <ArrowRight className="group-hover:translate-x-0.5 transition w-4 h-4 " />
+          مشاهده همه
+          <ArrowRight className="group-hover:translate-x-0.5 transition w-4 h-4" />
         </button>
       </div>
 
-      <div></div>
+      <div className="flex flex-wrap max-sm:justify-center gap-8 mt-8">
+        {dummyShowsData.slice(0, 4).map((show) => (
+          <MovieCard key={show._id} movie={show} />
+        ))}
+      </div>
 
       <div className="flex justify-center mt-20">
         <button
@@ -29,7 +39,7 @@ export default function FeaturedSection() {
           }}
           className="px-10 py-3 text-sm bg-primary hover:bg-primary-dull transition rounded-md font-medium cursor-pointer"
         >
-          Show more
+          نمایش بیشتر
         </button>
       </div>
     </div>

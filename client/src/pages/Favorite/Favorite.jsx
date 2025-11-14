d8933085845b427165018ff2176d1787cb9fe746
HosseinDakhili <hosseindakhili94@gmail.com>
2025-11-14 18:25:35 +0330

    feat: implement Favorite and MovieDetails pages with dynamic data rendering and loading states

--- client/src/pages/Favorite/Favorite.jsx
+++ client/src/pages/Favorite/Favorite.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { dummyShowsData } from "../../assets/assets";
+import BlurCircle from "../../components/BlurCircle";
+import MovieCard from "../Home/FeaturedSection/MovieCard";
 
 const Favorite = () => {
-  return (
-    <div></div>
-  )
-}
+  return dummyShowsData.length > 0 ? (
+    <div className="relative  my-40 mb-60 px-6 md:px-16 lg:px-20 xl:px-24 overflow-hidden min-h-[80vh]">
+      <BlurCircle top="150px" left="0" />
+      <BlurCircle bottom="50px" right="50px" />
+      <h2 className="text-lg font-medium my-4"> فیلم های مورد علاقه شما</h2>
+      <div className="flex flex-wrap   max-sm:justify-center gap-8">
+        {dummyShowsData.map((movie) => (
+          <MovieCard movie={movie} key={movie._id} />
+        ))}
+      </div>
+    </div>
+  ) : (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h2 className="text-3xl font-bold text-center">هیج فیلمی موجود نیست</h2>
+    </div>
+  );
+};
 
-export default Favorite
+export default Favorite;
